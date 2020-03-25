f613796b59cd10e3ddb3741122bb94c7558e7169
Martin Earl Raquion <martin.raquion@boom.camp>
2020-03-25 17:41:23 +0800

    Updates

---
+++ precache-manifest.6cc285c6d04c8d2dc884d735f52c7cae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bf07395d5b30db3afb1e81396934129",
+    "url": "/momentum_clone/index.html"
+  },
+  {
+    "revision": "6dbf7b12817832b01472",
+    "url": "/momentum_clone/static/css/main.be51b65e.chunk.css"
+  },
+  {
+    "revision": "131174a39e7bfdb5e7b7",
+    "url": "/momentum_clone/static/js/2.27c8b114.chunk.js"
+  },
+  {
+    "revision": "4214db9a6c23cd334176e62fe82579f5",
+    "url": "/momentum_clone/static/js/2.27c8b114.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dbf7b12817832b01472",
+    "url": "/momentum_clone/static/js/main.d75618d3.chunk.js"
+  },
+  {
+    "revision": "4e6ebf599df2ba7a54cf",
+    "url": "/momentum_clone/static/js/runtime-main.96815acd.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/momentum_clone/precache-manifest.6cc285c6d04c8d2dc884d735f52c7cae.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/momentum_clone/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
